634dc830b313389ca02df29efdf453f194b6d007
riki-araki <riki.araki@epkotsoftware.co.jp>
2022-08-01 22:58:58 +0900

    first commit

---
+++ app/Console/Commands/testcommand.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class testcommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+
+     //コマンド名
+    protected $signature = 'test:test';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        echo 1;
+        return 0;
+    }
+}

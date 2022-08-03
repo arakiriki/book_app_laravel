78897282c6969ec21fc72db000850dcb02d1523f
arakiriki <szn0125@gmail.com>
2022-08-03 12:02:37 +0900

    section9 create delete

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -9,12 +9,14 @@ class Book extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['name', 'status'];
+
     /**
      * id:一意（autoincrement）,not null
      * name: length 255, not null,
      * status: tinyInteger(1-255), not null, 1:読書中 2:未読 3:読破 4:読みたい
-     * author:著者 length 255, not ok.
-     * pabulication:出版社 length 255, not ok.
+     * author:著者 length 255, null ok.
+     * pabulication:出版社 length 255, null ok.
      * read_at: 読み終わった日 date(yyyy-mm-dd), null ok
      * note:　メモ text, null ok
      * timestomps:updated_at,created_at
--- resources/views/book/new.blade.php
+++ resources/views/book/new.blade.php
@@ -14,22 +14,21 @@
                     <h3 class="text-lg leading-6 font-medium text-gray-900">詳細</h3>
                   </div>
                   
-                  <form class="w-full" action="{{ route('book.update') }}" method="POST">
+                  <form class="w-full" action="{{ route('book.create') }}" method="POST">
                     @csrf
-                    @method('PATCH')
 
                     <div class="border-t border-gray-200">
                       <dl>
                         <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                           <dt class="text-sm font-medium text-gray-500">ID</dt>
-                          <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{{ $book->id }}</dd>
-                          <input value="{{ $book->id }}" name="id" type="hidden">
+                          <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2"></dd>
+                          <input value="" name="id" type="hidden">
                         </div>
                         <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                           <dt class="text-sm font-medium text-gray-500">名前</dt>
                           <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
                             <div class="col-span-6 sm:col-span-3">
-                              <input type="text" name="name" id="name" value="{{$book->name}}" autocomplete="given-name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                              <input type="text" name="name" id="name" value="" autocomplete="given-name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
                             </div>
                           </dd>
                         </div>
@@ -38,7 +37,7 @@
                           <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
                             <select id="status" name="status" autocomplete="country-name" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
                               @foreach(App\Models\Book::BOOK_STATUS_OBJECT as $key => $value)
-                                <option value="{{ $key }}" @if($key === $book->status) selected @endif>{{ $value }}</option>
+                                <option value="{{ $key }}">{{ $value }}</option>
                               @endforeach
                             </select>
                           </dd>
@@ -47,7 +46,7 @@
                           <dt class="text-sm font-medium text-gray-500">著者</dt>
                           <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
                             <div class="col-span-6 sm:col-span-3">
-                              <input type="text" name="author" id="author" value="{{$book->author}}" autocomplete="given-name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                              <input type="text" name="author" id="author" value="" autocomplete="given-name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
                             </div>
                           </dd>
                         </div>
@@ -55,7 +54,7 @@
                           <dt class="text-sm font-medium text-gray-500">出版社</dt>
                           <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
                             <div class="col-span-6 sm:col-span-3">
-                              <input type="text" name="publication" id="publication" value="{{$book->publication}}" autocomplete="given-name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                              <input type="text" name="publication" id="publication" value="" autocomplete="given-name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
                             </div>
                           </dd>
                         </div>
@@ -63,7 +62,7 @@
                           <dt class="text-sm font-medium text-gray-500">読み終わった日</dt>
                           <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
                             <div class="col-span-6 sm:col-span-3">
-                              <input type="text" name="read_at" id="read_at" value="{{$book->read_at}}" placeholder="例）2022-01-01" autocomplete="given-name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                              <input type="text" name="read_at" id="read_at" value="" placeholder="例）2022-01-01" autocomplete="given-name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
                             </div>
                           </dd>
                         </div>
@@ -71,7 +70,7 @@
                           <dt class="text-sm font-medium text-gray-500">メモ</dt>
                           <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
                             <div class="mt-1">
-                              <textarea id="note" name="note" rows="5" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md">{{$book->note}}"</textarea>
+                              <textarea id="note" name="note" rows="5" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"></textarea>
                             </div>
                           </dd>
                         </div>
@@ -80,7 +79,7 @@
                   </div>
                     <div class="flex justify-center">
                       <button onclick="history.back()" class="mt-4 mr-4 shadow bg-gray-500 hover:bg-gray-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">{{ __('戻る') }}</button> 
-                      <button onclick="location.href='/book/edit/{{ $book->id }}'" class="mt-4 mr-2 shadow bg-orange-500 hover:bg-orange-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded">変更</button> 
+                      <button type="submit" class="mt-4 mr-2 shadow bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded">新規作成</button> 
                     </div>
 
                   </form>
